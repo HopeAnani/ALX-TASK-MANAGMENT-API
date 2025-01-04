3baad3fb7f89e823b44390a9bae0b1c8e0ec07b2
Anani Samuel <ananisamuelhope@gmail.com>
2025-01-04 22:35:50 +0300

    requirements.txt

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p*d$#b2b_4rs@wvwvmed*!ce7hq_rujo&p7z50!^%kha=4#azx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['HopeAnani.pythonanywhere.com']
+
 
 AUTH_USER_MODEL = 'account.User'
 
