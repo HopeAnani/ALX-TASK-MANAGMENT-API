327249dbdf58565c18dca3178b336b8465ba63ff
Anani Samuel <ananisamuelhope@gmail.com>
2025-01-15 10:14:54 +0300

    url provided

--- taskmanager/account/urls.py
+++ taskmanager/account/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
-    path('api/signup/', SignUpAPIView.as_view(), name='signup'),
-    path('api/signin/', SignInAPIView.as_view(), name='signin'),
-    path('api/signout/', SignOutAPIView.as_view(), name='signout'),
+    path('api/signup/', SignUpAPIView.as_view(), name='apisignup'),
+    path('api/signin/', SignInAPIView.as_view(), name='apisignin'),
+    path('api/signout/', SignOutAPIView.as_view(), name='apisignout'),
 ]
